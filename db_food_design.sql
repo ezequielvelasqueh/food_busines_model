6d2a6a3891572e0c37cecab3e45fbef0c720500b
ezequielvelasqueh <ezequiel.velasque@pucp.pe>
2023-10-20 22:33:35 -0500

    First version of a simple model of food business (restaurant)

---
+++ db_food_design.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE food_business;
+USE food_business;
+CREATE TABLE customer(
+customer_id smallint unsigned,
+customer_id_type varchar(20),
+addres varchar(50),
+district_id int,
+
+ 
+);

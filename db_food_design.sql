dd0e16e0bb0c221636e820c1d496e0550504af96
ezequielvelasqueh <ezequiel.velasque@pucp.pe>
2023-10-20 23:05:59 -0500

    update from first version. Consumer Table

--- db_food_design.sql
+++ db_food_design.sql
@@ -3,7 +3,10 @@ USE food_business;
 CREATE TABLE customer(
 customer_id smallint unsigned,
 customer_id_type varchar(20),
-addres varchar(50),
+customer_firstname varchar(20),
+customer_lastname1 varchar(20),
+customer_lastname2 varchar(20),
+address varchar(50),
 district_id int,
 
  
